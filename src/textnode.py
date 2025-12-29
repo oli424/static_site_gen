fdfcad67f60c556a76197bf73455ddc89d287ad1
Olivier Gaudet <gaudet424@gmail.com>
2025-12-29 16:44:05 -0500

    0.1 - inital setup and textnode class

---
+++ src/textnode.py
@@ -0,0 +1,32 @@
+from enum import Enum
+
+class TextType(Enum):
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+
+class TextNode():
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = TextType(text_type)
+        self.url = url
+
+    def __eq__(self, other):
+        if self == other:
+            return True
+        return False
+    
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    
+
+def main():
+    dummy = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    print(dummy)
+
+if __name__ == "__main__":
+    main()
